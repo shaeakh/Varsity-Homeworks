f1e9b343609ab3e4606cefa233f3a1d75c411141
Md. Shaeakh Ahmed Chowdhury <102303488+shaeakh@users.noreply.github.com>
2022-07-19 08:02:16 +0600

    Update consistent batsman.c

--- consistent batsman.c
+++ consistent batsman.c
@@ -12,7 +12,7 @@ int main()
     double mew1 = (bm1[0].x+bm1[1].x+bm1[2].x+bm1[3].x+bm1[4].x)/5;
     double mew2 = (bm2[0].x+bm2[1].x+bm2[2].x+bm2[3].x+bm2[4].x)/5;
     double n=0,x=0,sum1=0,sum2=0;
-   for(int i=0;i<5;i++){
+    for(int i=0;i<5;i++){
             n=bm1[i].x - mew1;
             x=n*n;
             sum1=sum1+x;
@@ -27,7 +27,7 @@ int main()
     double sample2 = sqrt( sum2/4 );
     if(sigma1<sigma2){
         printf("Batman No 01 is the best player. Stanfard Deviation of his runs are %lf\n",sigma1);
-                printf("Which is the lowest Stanford deviation.\nwhile the Stanfard Deviation of other player is %lf\n",sigma2);
+        printf("Which is the lowest Stanford deviation.\nwhile the Stanfard Deviation of other player is %lf\n",sigma2);
     }
     else{
         printf("Batman No 02 is the best player. Stanfard Deviation of his runs are %lf\n",sigma2);
